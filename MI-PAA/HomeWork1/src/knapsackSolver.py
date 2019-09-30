b53ab533149fa5f6239f95004873d72944ae9856
budikpet <sgt.petrov@gmail.com>
2019-09-30 19:13:43 +0200

    knapsackSolver interface done.

--- MI-PAA/HomeWork1/src/knapsackSolver.py
+++ MI-PAA/HomeWork1/src/knapsackSolver.py
@@ -3,29 +3,24 @@ import os
 from myDataClasses import Task, Solution
 import solverStrategy
 
-class Context():
-
-    def __init__(self, strategy):
-        self.strategy = strategy
-
-    def solve(self, task: Task):
-        self.strategy.solve(task)
-
 @click.command()
 @click.option("--dataFile", type=click.File("r"), required=True)
 def knapsackSolver(datafile):
     data = datafile.readline()
-    context = Context(solverStrategy.BranchBorder())
+    context = solverStrategy.Context(solverStrategy.BranchBorder())
+    solutions = list()
 
     while data:
         values = data.split(" ")
         task = Task(id=values.pop(0), count=values.pop(0), capacity=values.pop(0), minValue=values.pop(0), thingValues=values)
 
-        context.solve(task)
+        solution = context.solve(task)
+        print(solution)
+        solutions.append(solution)
 
         data = datafile.readline()
 
-    print
+    return solutions
 
 
 if __name__ == "__main__":
--- MI-PAA/HomeWork1/src/solverStrategy.py
+++ MI-PAA/HomeWork1/src/solverStrategy.py
@@ -1,17 +1,29 @@
 from myDataClasses import Task, Solution
 
+class Context():
+
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def solve(self, task: Task) -> Solution:
+        return self.strategy.solve(task)
+
 class SolverStrategy(object):
     
-    def solve(self, task: Task):
+    def solve(self, task: Task) -> Solution:
         pass
 
 
 class BruteForce(SolverStrategy):
     
-    def solve(self, task: Task):
+    def solve(self, task: Task) -> Solution:
         print(f"BruteForce#{task.id} solving.")
+
+        return Solution(-1, -1, -1, [])
 
 class BranchBorder(SolverStrategy):
     
-    def solve(self, task: Task):
+    def solve(self, task: Task) -> Solution:
         print(f"BranchBorder#{task.id} solving.")
+
+        return Solution(-1, -1, -1, [])
