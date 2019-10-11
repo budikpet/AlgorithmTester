26ed1c867bd71d62792d11ff9eed6f5efb554a4b
Petr Budik <sgt.petrov@gmail.com>
2019-10-11 22:54:39 +0200

    Possible to call for 1 file only.

--- MI-PAA/HomeWork1/src/output_generator.py
+++ MI-PAA/HomeWork1/src/output_generator.py
@@ -4,21 +4,46 @@ from typing import Dict
 import os
 from solverStrategy import Strategies
 from myDataClasses import Modes
-from helpers import get_files_dict, create_clean_folder
+from helpers import get_files_dict
+
+def create_path(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+@click.group()
+def generate_output():
+    pass
 
 inputStrategies = [strategy.name for strategy in Strategies]
 inputModes = [mode.name for mode in Modes]
 
-@click.command()
+@generate_output.command()
+@click.option("-s", "--strategy", type=click.Choice(inputStrategies), default=inputStrategies[0], show_default=True)
+@click.option("-f", "--input-file", type=click.File("r"), required=True)
+@click.argument("output-dir", required=True)
+def file(strategy, input_file, output_dir):
+    program = "/home/petr/Documents/LocalShared/PythonSamples/MI-PAA/HomeWork1/src/knapsackSolver.py"
+    create_path(output_dir)
+
+    # Run command
+    p = Popen(["python", program, "--dataFile", input_file.name, "-s", strategy], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+
+    output_file_name = input_file.name.split("/")[-1].replace(".dat", f'_{strategy}.dat')
+    print(f'Running output for: {output_file_name}')
+    with open(f'{output_dir}/{output_file_name}', "w") as output_file:
+        for line in p.stdout:
+            output_file.write(line.decode("utf-8"))
+            output_file.flush()
+
+@generate_output.command()
 @click.option("-s", "--strategy", type=click.Choice(inputStrategies), default=inputStrategies[0], show_default=True)
 @click.option("--start-count", type=int, default=4)
 @click.option("--end-count", type=int, default=42)
 @click.argument("input-dir", required=True)
 @click.argument("output-dir", required=True)
-def generate_output(strategy, start_count, end_count, input_dir, output_dir):
+def files(strategy, start_count, end_count, input_dir, output_dir):
     program = "/home/petr/Documents/LocalShared/PythonSamples/MI-PAA/HomeWork1/src/knapsackSolver.py"
-
-    create_clean_folder(output_dir)
+    create_path(output_dir)
 
     files_dict = get_files_dict(input_dir)
 
