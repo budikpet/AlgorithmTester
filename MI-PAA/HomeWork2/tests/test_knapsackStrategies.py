873a8806da7547df3899b5114cec37cd110b9900
budikpet <sgt.petrov@gmail.com>
2019-10-20 18:19:16 +0200

    Bugfixes.

--- MI-PAA/HomeWork2/tests/test_knapsackStrategies.py
+++ MI-PAA/HomeWork2/tests/test_knapsackStrategies.py
@@ -1,12 +1,12 @@
 from knapsackSolver import knapsackSolver
-from solverStrategy import Strategies, Modes
+from solverStrategy import Strategies
 from myDataClasses import Solution
 from helpers import FilePair, getFiles
 from click.testing import CliRunner
 
-def checkFile(cliRunner, filePair: FilePair, strategy, mode):
+def checkFile(cliRunner, filePair: FilePair, strategy):
     print(f"Testing file: {filePair.dataFile}")
-    solutions = cliRunner.invoke(knapsackSolver, ["--dataFile", filePair.dataFile, "-s", strategy.name, "--mode", mode.name]).output.split("\n")
+    solutions = cliRunner.invoke(knapsackSolver, ["--dataFile", filePair.dataFile, "-s", strategy.name]).output.split("\n")
 
     # Check values
     with open(filePair.solutionFile, "r") as solutionFile:
@@ -14,27 +14,23 @@ def checkFile(cliRunner, filePair: FilePair, strategy, mode):
             if solution == "":
                 break
             line = solutionFile.readline().split(" ")
-            solution = solution.split(" ")
+            solution = solution.split(";")
 
             if len(line) == 1:
                 return
 
-            # solution = what the programme calculated
-            # line = what we got from the solution's file
-            assert int(solution[1]) <= pow(2, int(solution[2]))
-
-            if mode == Modes.Constructive:
-                assert int(line[2]) == int(solution[3])
+            # Check if found value matches
+            assert int(line[2]) == int(solution[2])
             print
 
 def test_constructive_DP_NK():
     path = './data'
     cliRunner = CliRunner()
 
-    dataFiles = getFiles(f'{path}/NK')[0:2]
+    dataFiles = getFiles(f'{path}/NK')[0:4]
     
     for filePair in dataFiles:
-        checkFile(cliRunner, filePair, Strategies.DP, Modes.Constructive)
+        checkFile(cliRunner, filePair, Strategies.DP)
 
 def test_constructive_GreedySimple_NK():
     path = './data'
@@ -43,7 +39,7 @@ def test_constructive_GreedySimple_NK():
     dataFiles = getFiles(f'{path}/NK')[0:2]
     
     for filePair in dataFiles:
-        checkFile(cliRunner, filePair, Strategies.GreedySimple, Modes.Constructive)
+        checkFile(cliRunner, filePair, Strategies.GreedySimple)
 
 def test_constructive_Greedy_NK():
     path = './data'
@@ -52,7 +48,7 @@ def test_constructive_Greedy_NK():
     dataFiles = getFiles(f'{path}/NK')[0:2]
     
     for filePair in dataFiles:
-        checkFile(cliRunner, filePair, Strategies.Greedy, Modes.Constructive)
+        checkFile(cliRunner, filePair, Strategies.Greedy)
 
 def test_constructive_FPTAS_NK():
     path = './data'
@@ -61,4 +57,4 @@ def test_constructive_FPTAS_NK():
     dataFiles = getFiles(f'{path}/NK')[0:2]
     
     for filePair in dataFiles:
-        checkFile(cliRunner, filePair, Strategies.FPTAS, Modes.Constructive)
+        checkFile(cliRunner, filePair, Strategies.FPTAS)
