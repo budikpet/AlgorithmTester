58aea91129fd2659f739352986a3a88e94f2859d
budikpet <sgt.petrov@gmail.com>
2019-12-10 10:50:04 +0100

    Documentation

--- algorithm_tester/decorators.py
+++ algorithm_tester/decorators.py
@@ -1,4 +1,5 @@
 def docstring_parameters(*args, **kwargs):
+    """ A decorator that enables parameterized docstring. """
     def dec(obj):
         obj.__doc__ = obj.__doc__.format(*args, **kwargs)
         return obj
