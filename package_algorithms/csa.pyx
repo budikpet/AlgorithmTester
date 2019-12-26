416924ab907906540ec8ce8fc2007d27a774cb26
budikpet <sgt.petrov@gmail.com>
2019-12-26 23:31:53 +0100

    Further cython performance improvements.
    SA algorithm is now about 17x faster.

--- package_algorithms/csa.pyx
+++ package_algorithms/csa.pyx
@@ -1,7 +1,7 @@
-import numpy as np
 cimport numpy as np
 cimport cython
-from math import exp
+from libc.math cimport exp
+from libc.stdlib cimport rand, srand, RAND_MAX
 
 ctypedef np.int64_t TYPE
 
@@ -51,38 +51,20 @@ cdef (int, int) get_new_neighbour(long[:] solution, int cost_sum, int weight_sum
 
     return cost_sum, weight_sum
 
-# while curr_temp > task.min_temp:
-#     rand_indexes: np.ndarray = np.random.randint(0, task.count-1, size=task.cycles, dtype=int)
-#     rand_exp_predicates: np.ndarray = np.random.uniform(size=task.cycles)
-#     for cycle in range(task.cycles):
-#         sol_cntr += 1
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef float random_float():
+    return rand()/RAND_MAX
 
-#         # Try neighbour solution
-#         csa.get_new_neighbour(neighbour_sol.solution, neighbour_sol.sum_cost, neighbour_sol.sum_weight, 
-#             index=rand_indexes[cycle], capacity=task.capacity, count=task.count, costs=costs, weights=weights)
-#         neighbour_fitness: float = self.get_fitness(task, neighbour_sol)
-
-#         if neighbour_fitness > best_fitness:
-#             # Neighbour solution is better, accept it
-#             best_fitness = neighbour_fitness
-#             best_sol.copy(neighbour_sol)
-
-#         elif exp( (neighbour_fitness - best_fitness) / curr_temp) >= rand_exp_predicates[cycle]:
-#             # Simulated Annealing condition. 
-#             # Enables us to accept worse solution with a certain probability
-#             best_fitness = neighbour_fitness
-#             best_sol.copy(neighbour_sol)
-
-#         else:
-#             # Change the solution back
-#             neighbour_sol.copy(best_sol)
-#         print
-
-#     curr_temp *= task.cooling_coefficient
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef int random_int(int low = 0, int height = 2):
+    return rand() % height + low
 
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexes checking
 @cython.initializedcheck(False)
+@cython.cdivision(True)
 cpdef (int, int, int) get_solution(long[:] solution, int sum_cost, int sum_weight, float init_temp, float min_temp, float cooling_coef, int cycles, int capacity, long[:] costs, long[:] weights):
     cdef long[:] best_sol, neighbour_sol, rand_indexes
     cdef double[:] rand_exp_predicates
@@ -97,15 +79,15 @@ cpdef (int, int, int) get_solution(long[:] solution, int sum_cost, int sum_weigh
     best_sol = solution.copy()
     neighbour_sol = solution.copy()
 
+    srand(20191219)
+    
     while curr_temp > min_temp:
-        rand_indexes = np.random.randint(0, count-1, size=cycles, dtype=int)
-        rand_exp_predicates = np.random.uniform(size=cycles)
         for cycle in range(cycles):
             sol_cntr += 1
 
             # Try neighbour solution
             get_new_neighbour(neighbour_sol, neighbour_cost, neighbour_weight,
-                index=rand_indexes[cycle], 
+                index=random_int(0, count - 1), 
                 capacity=capacity, count=count, costs=costs, weights=weights)
 
             if neighbour_cost > best_cost:
@@ -114,7 +96,7 @@ cpdef (int, int, int) get_solution(long[:] solution, int sum_cost, int sum_weigh
                 best_cost = neighbour_cost
                 best_weight = neighbour_weight
 
-            elif exp( (neighbour_cost - best_cost) / curr_temp) >= rand_exp_predicates[cycle]:
+            elif exp( (neighbour_cost - best_cost) / curr_temp) >= random_float():
                 # Simulated Annealing condition. 
                 # Enables us to accept worse solution with a certain probability
                 best_sol = neighbour_sol.copy()
