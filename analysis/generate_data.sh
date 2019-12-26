8d440912c46a948b7c452c43e6a6f1c299338094
budikpet <sgt.petrov@gmail.com>
2019-12-26 13:35:24 +0100

    Prepared performance test.

--- analysis/generate_data.sh
+++ analysis/generate_data.sh
@@ -1,14 +1,5 @@
 
 # Init temperature
-<<<<<<< Updated upstream
-OUT_DIR="analysis/tester_results/PARAM_ANALYSIS/INIT_TEMPERATURE"
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/100 --init-temperature 100 --min-temperature 1 --cooling 0.995 --cycles 50
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/300 --init-temperature 300 --min-temperature 1 --cooling 0.995 --cycles 50
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/500 --init-temperature 500 --min-temperature 1 --cooling 0.995 --cycles 50
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/700 --init-temperature 700 --min-temperature 1 --cooling 0.995 --cycles 50
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/900 --init-temperature 900 --min-temperature 1 --cooling 0.995 --cycles 50
-run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/PARAM_ANALYSIS --output-dir $OUT_DIR/1000 --init-temperature 1000 --min-temperature 1 --cooling 0.995 --cycles 50
-=======
 echo "Initial temperature"
 OUT_DIR="analysis/tester_results/ParamAnalysis/InitTemperature"
 run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/ParamAnalysis --output-dir $OUT_DIR/100 --init-temperature 100 --min-temperature 1 --cooling 0.995 --cycles 50
@@ -37,4 +28,3 @@ run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/
 run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/ParamAnalysis --output-dir $OUT_DIR/200 --init-temperature 500 --min-temperature 1 --cooling 0.950 --cycles 50
 run_tester -s SA,SAPenalty --check-time True -p KnapsackParser --input-dir data/ParamAnalysis --output-dir $OUT_DIR/250 --init-temperature 500 --min-temperature 1 --cooling 0.975 --cycles 50
 
->>>>>>> Stashed changes
