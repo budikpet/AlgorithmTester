25a808124aa757423e21535d3c2432457868c4f2
budikpet <sgt.petrov@gmail.com>
2019-12-28 00:11:26 +0100

    Added old SA for testing purposes only.

--- package_algorithms/__init__.py
+++ package_algorithms/__init__.py
@@ -3,6 +3,7 @@ from package_algorithms.dp import DynamicProgramming, DynamicProgramming_Weight
 from package_algorithms.basic import BruteForce, Greedy
 from package_algorithms.sa import SimulatedAnnealing
 from package_algorithms.sa_penalty import SimulatedAnnealingPenalty
+from package_algorithms.sa_old import SimulatedAnnealing_OLD
 
-__plugins__ = [BranchBound, BruteForce, DynamicProgramming, DynamicProgramming_Weight, Greedy, SortedBranchBound, SimulatedAnnealing, SimulatedAnnealingPenalty]
+__plugins__ = [BranchBound, BruteForce, DynamicProgramming, DynamicProgramming_Weight, Greedy, SortedBranchBound, SimulatedAnnealing, SimulatedAnnealingPenalty, SimulatedAnnealing_OLD]
 __all__ = [plugin.__name__ for plugin in __plugins__]
