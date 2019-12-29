a5866ac72ca22012f0830eecd2929b451c998ff3
budikpet <sgt.petrov@gmail.com>
2019-12-29 12:26:12 +0100

    Update

--- test_data_analysis2.sh
+++ test_data_analysis2.sh
@@ -1,15 +1,1 @@
-path="analysis/tester_results"
-ALG="SA"
-
-# Init temperature
-echo "Initial temperature"
-OUT_DIR=$path"/ParamAnalysis/InitTemperature"
-
-step=100
-start=3500
-end=4000
-
-for i in `seq $start $step $end`; do 
-    echo $i
-    run_tester -s $ALG --check-time True -p KnapsackParser --input-dir data/ParamAnalysis --output-dir $OUT_DIR/$i --init-temperature $i --min-temperature 1 --cooling 0.995 --cycles 50
-done
+run_tester -s SA --check-time True -p KnapsackParser --input-dir data/EvoFile --output-dir analysis/tester_results/EvoFile --init-temperature 2500 --min-temperature 1 --cooling 0.950 --cycles 50 --create-evo-file True
