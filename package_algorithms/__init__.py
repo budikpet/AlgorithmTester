09f52077e4064793c916425d0160e4d07aa0a117
budikpet <sgt.petrov@gmail.com>
2020-01-01 23:35:09 +0100

    Moved knapsack algorithms into a separate package.

--- package_algorithms/__init__.py
+++ package_algorithms/__init__.py
@@ -1,9 +1,9 @@
-from package_algorithms.bb import BranchBound, SortedBranchBound
-from package_algorithms.dp import DynamicProgramming, DynamicProgramming_Weight
-from package_algorithms.basic import BruteForce, Greedy
-from package_algorithms.sa import SimulatedAnnealing
-from package_algorithms.sa_penalty import SimulatedAnnealingPenalty
-from package_algorithms.sa_old import SimulatedAnnealing_OLD
+from package_algorithms.knapsack.bb import BranchBound, SortedBranchBound
+from package_algorithms.knapsack.dp import DynamicProgramming, DynamicProgramming_Weight
+from package_algorithms.knapsack.basic import BruteForce, Greedy
+from package_algorithms.knapsack.sa import SimulatedAnnealing
+from package_algorithms.knapsack.sa_penalty import SimulatedAnnealingPenalty
+from package_algorithms.knapsack.sa_old import SimulatedAnnealing_OLD
 
 __plugins__ = [BranchBound, BruteForce, DynamicProgramming, DynamicProgramming_Weight, Greedy, SortedBranchBound, SimulatedAnnealing, SimulatedAnnealingPenalty, SimulatedAnnealing_OLD]
 __all__ = [plugin.__name__ for plugin in __plugins__]
