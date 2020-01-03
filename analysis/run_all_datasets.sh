f3dda80801a97963c802869aad098cd1ec42923e
Petr Budik <sgt.petrov@gmail.com>
2020-01-03 14:29:33 +0100

    First results.

---
+++ analysis/run_all_datasets.sh
@@ -0,0 +1,68 @@
+ALG="SA_SAT"
+CONC="FILES"
+
+init_temp=2500
+min_temp=1
+cycles=50
+cooling=0.995
+
+# wufA
+
+PARENT="wuf-A"
+DATASET="wuf20-88-A"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+DATASET="wuf20-91-A"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# DATASET="wuf100-430-A"
+# run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# wufM
+
+PARENT="wuf-M"
+DATASET="wuf20-78-M"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+DATASET="wuf50-201-M"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# DATASET="wuf75-310-M"
+# run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# wufN
+
+PARENT="wuf-N"
+DATASET="wuf20-78-N"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+DATASET="wuf50-201-N"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# DATASET="wuf75-310-N"
+# run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# wufQ
+
+PARENT="wuf-Q"
+DATASET="wuf20-78-Q"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+DATASET="wuf50-201-Q"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# DATASET="wuf75-310-Q"
+# run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# wufR
+
+PARENT="wuf-R"
+DATASET="wuf20-78-R"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+DATASET="wuf50-201-R"
+run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
+# DATASET="wuf75-310-R"
+# run_tester --input-dir data/$PARENT/$DATASET --output-dir analysis/tester_results/$PARENT/$DATASET -s $ALG -r $CONC --check-time True -p "SATParser" --init-temperature $init_temp --min-temperature $min_temp --cycles $cycles --cooling $cooling
+
