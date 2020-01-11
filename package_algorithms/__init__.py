1be8ad1fbc9b8823647115f6e77a9fc72ca86a79
budikpet <sgt.petrov@gmail.com>
2020-01-11 14:31:22 +0100

    Created SA_SAT_V3

--- package_algorithms/__init__.py
+++ package_algorithms/__init__.py
@@ -6,7 +6,8 @@ from package_algorithms.knapsack.sa_penalty import SimulatedAnnealingPenalty
 from package_algorithms.knapsack.sa_old import SimulatedAnnealing_OLD
 from package_algorithms.sat.sa_sat_v1 import SimulatedAnnealing_SAT_V1
 from package_algorithms.sat.sa_sat_v2 import SimulatedAnnealing_SAT_V2
+from package_algorithms.sat.sa_sat_v3 import SimulatedAnnealing_SAT_V3
 
 # __plugins__ = [BranchBound, BruteForce, DynamicProgramming, DynamicProgramming_Weight, Greedy, SortedBranchBound, SimulatedAnnealing, SimulatedAnnealingPenalty, SimulatedAnnealing_OLD]
-__plugins__  = [SimulatedAnnealing_SAT_V1, SimulatedAnnealing_SAT_V2]
+__plugins__  = [SimulatedAnnealing_SAT_V1, SimulatedAnnealing_SAT_V2, SimulatedAnnealing_SAT_V3]
 __all__ = [plugin.__name__ for plugin in __plugins__]
